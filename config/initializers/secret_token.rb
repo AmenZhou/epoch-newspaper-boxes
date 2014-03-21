9aae82a26d57b90ee2c0fd127157c7696420f8c9
Herman <chou_amen@hotmail.com>
2014-03-21 17:27:37 -0400

    Create epoch newspaper count

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EpochNewspaperBoxes::Application.config.secret_key_base = 'e84df09d5e865363ba9516c6a978dd23c0eef228b07b22dd21fb64d0229037e454b384f2fdd8c86b9199b66a171f810ec637bcbf943f8cd5af88d15bbe9bd6c7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EpochNewspaperBoxes::Application.config.session_store :cookie_store, key: '_epoch-newspaper-boxes_session'
