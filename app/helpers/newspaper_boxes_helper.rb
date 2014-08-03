66ef3a1cc022e5d34c4e0ddb2222a94053bc7b90
Ken Lu <ken@seeice.com>
2014-08-03 21:17:52 +0000

    change deliver type to select option

--- app/helpers/newspaper_boxes_helper.rb
+++ app/helpers/newspaper_boxes_helper.rb
@@ -8,4 +8,11 @@ module NewspaperBoxesHelper
     output = zipcode_report.drop(1).map{|rs| [rs[:zipcode].to_s, rs[:mon], rs[:tue], rs[:wed], rs[:thu], rs[:fri], rs[:sat], rs[:sun]]}
     output.unshift(['zipcode', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'])
   end
+
+  def deliver_type newspaper_boxes
+    #[[1, "Spain"], [2, "Italy"], [3, "Germany"], [4, "France"]]
+    #NewspaperBoxes.pluck(:deliver_type)
+    @delivery_types ||= newspaper_boxes.pluck(:deliver_type).compact.uniq.map{|np| [np, np]}
+  end
+ 
 end
