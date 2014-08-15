f72f95b0b9cd8d05ae7f5ec69f1693efcea65ae0
Ken Lu <ken@seeice.com>
2014-08-15 19:30:56 +0000

    sales can not see paper count hover over icon, change website title

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   def is_admin?
-    true if self.role == 'Admin'
+    self.role == 'Admin' ? true : false
   end
 end
