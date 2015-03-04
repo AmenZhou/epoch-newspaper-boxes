893abac63a7728bc15eee5d26100e6338a9dbc08
AmenZhou <chou.amen@gmail.com>
2015-03-04 23:17:48 +0000

    refactor chart partials

--- app/helpers/newspaper_boxes_helper.rb
+++ app/helpers/newspaper_boxes_helper.rb
@@ -1,4 +1,25 @@
 module NewspaperBoxesHelper
+  def charts_array
+    [ { title: 'Boroughs', report: @report, chart_id: 'piechart_3d'},
+      { title: 'Queens Areas', report: @report_queens, chart_id: 'piechart2_3d'},
+      { title: 'Boroughs Weekday Average', report: @weekday_average, chart_id: 'piechart3_3d'},
+      { title: 'Boroughs Weekend Average', report: @weekend_average, chart_id: 'piechart4_3d'},
+      { title: 'Boroughs Friday', report: @fri, chart_id: 'piechart5_3d'},
+      { title: 'Boroughs Saturday', report: @sat, chart_id: 'piechart6_3d'},
+      { title: 'Deliver Type Percentage', report: @all_deliver_type_percentage, chart_id: 'piechart7_3d'},
+      { title: 'Queens Deliver Type Percentage', report: @queens_deliver_type_percentage, chart_id: 'piechart8_3d'},
+      { title: 'Manhattan Deliver Type Percentage', report: @manhattan_deliver_type_percentage, chart_id: 'piechart9_3d'},
+      { title: 'Brooklyn Deliver Type Percentage', report: @brooklyn_deliver_type_percentage, chart_id: 'piechart10_3d'},
+      { title: 'New Jersey Deliver Type Percentage', report: @newjersey_deliver_type_percentage, chart_id: 'piechart11_3d'}
+    ]
+  end
+
+  def chart_options
+    charts_array.map do |chart|
+      [chart[:title], chart[:chart_id]]
+    end
+  end
+
   def chart_deliver_percentage reports
     output = reports.map{|rs| [rs.group, rs.percentage]}
     output.unshift(['Delivery Type', 'Percentage'])
