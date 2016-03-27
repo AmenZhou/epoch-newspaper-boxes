1485eecb7697700b12efb28088bc1cff017d7d9b
amenzhou <chou.amen@gmail.com>
2016-03-27 16:05:30 +0000

    #28 add trend chart for box total amount

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -4,14 +4,25 @@ class HistoriesController < ApplicationController
   end
 
   def trend
+    #params[:filter] => weekly_delivery/box_amount
     box_type = params[:box_type] || 'NewspaperBox'
     histories = History.where(box_type: box_type,
-                              epoch_branch_id: current_user.epoch_branch_id).order('created_at').map do |h|
-      [
-        h.created_at.strftime('%Y-%m-%d'),
-        h.newspaper
-      ]
+                              epoch_branch_id: current_user.epoch_branch_id).order('created_at')
+    if params[:filter] == 'weekly_delivery'
+      data = histories.map do |h|
+        [
+          h.created_at.strftime('%Y-%m-%d'),
+          h.newspaper
+        ]
+      end
+    elsif params[:filter] == 'box_amount'
+      data = histories.map do |h|
+        [
+          h.created_at.strftime('%Y-%m-%d'),
+          h.box
+        ]
+      end
     end
-    render json: histories
+    render json: data
   end
 end
